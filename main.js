f7118562c98d492c0874821951c00a00de13acf8
Prudencio Chaparro <pchaparrog55@gmail.com>
2023-12-05 08:07:34 +0100

    Esto es un commit

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ createWindow = () => {
 
     appWin.setMenu(null);
 
-    appWin.webContents.openDevTools();
+    // appWin.webContents.openDevTools();
 
     appWin.on("closed", () => {
         appWin = null;
